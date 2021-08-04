d4fafc0029bc5043f390ae59ebee9a3ceaec012c
Corey Dubin <dubincorey@gmail.com>
2021-08-04 09:05:34 -0400

    pls work

---
+++ .cache/sync-requires.js
@@ -0,0 +1,11 @@
+
+// prefer default export if available
+const preferDefault = m => (m && m.default) || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\.cache\\dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\src\\pages\\404.js")),
+  "component---src-pages-index-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\src\\pages\\index.js"))
+}
+
