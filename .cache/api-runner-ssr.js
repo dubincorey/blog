6be208724efee91d9279006da25937974fdc1963
Corey Dubin <dubincorey@gmail.com>
2021-08-05 19:20:53 -0400

    part 6 done

--- .cache/api-runner-ssr.js
+++ .cache/api-runner-ssr.js
@@ -1,4 +1,12 @@
-var plugins = []
+var plugins = [{
+      name: 'gatsby-plugin-image',
+      plugin: require('C:/Users/Corey/Desktop/home-dashboard/node_modules/gatsby-plugin-image/gatsby-ssr'),
+      options: {"plugins":[]},
+    },{
+      name: 'gatsby-plugin-mdx',
+      plugin: require('C:/Users/Corey/Desktop/home-dashboard/node_modules/gatsby-plugin-mdx/gatsby-ssr'),
+      options: {"plugins":[],"extensions":[".mdx"],"defaultLayouts":{},"gatsbyRemarkPlugins":[],"lessBabel":false,"remarkPlugins":[],"rehypePlugins":[],"mediaTypes":["text/markdown","text/x-markdown"],"root":"C:\\Users\\Corey\\Desktop\\home-dashboard"},
+    }]
 /* global plugins */
 // During bootstrap, we write requires at top of this file which looks like:
 // var plugins = [
--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -6,6 +6,9 @@ const preferDefault = m => (m && m.default) || m
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\.cache\\dev-404-page.js")),
   "component---src-pages-404-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\src\\pages\\404.js")),
+  "component---src-pages-about-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\src\\pages\\about.js")),
+  "component---src-pages-blog-index-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\src\\pages\\blog\\index.js")),
+  "component---src-pages-blog-mdx-slug-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\src\\pages\\blog\\{mdx.slug}.js")),
   "component---src-pages-index-js": preferDefault(require("C:\\Users\\Corey\\Desktop\\home-dashboard\\src\\pages\\index.js"))
 }
 
