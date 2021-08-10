6dcd61eeff290edf3cf989d609d164a8c037ae15
Corey Dubin <dubincorey@gmail.com>
2021-08-09 21:26:04 -0400

    adjusted some spacing for h1 and p

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,15 +1,13 @@
 import * as React from "react";
 import Layout from "../components/layout";
-import { StaticImage } from "gatsby-plugin-image";
 
 const IndexPage = () => {
   return (
-    <Layout pageTitle="Home Page">
-      <p>I'm making this by following the Gatsby Tutorial.</p>
-      <StaticImage
-        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
-        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
-      />
+    <Layout pageTitle="QuickFi">
+      <p>
+        I'm going to share my experiences and understanding of personal finance
+        and (mostly) credit cards!
+      </p>
     </Layout>
   );
 };
