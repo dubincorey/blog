50ba5509e3b473e030f08557102dbb8140016160
Corey Dubin <dubincorey@gmail.com>
2021-08-10 10:36:45 -0400

    big changes, colors, design, layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -9,52 +9,33 @@ import {
   outerBorder,
   decor,
   innerBorder,
-  p,
 } from "../pages/layout.module.css";
 
 const Layout = ({ pageTitle, children }) => {
-  const data = useStaticQuery(graphql`
-    query {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `);
   return (
     <div>
-      <div className={`${outerBorder} ${decor}`}>
-        <div className={`${innerBorder} ${decor}`}>
-          <div className={container}>
-            <title>
-              {pageTitle} | {data.site.siteMetadata.title}
-            </title>
-            <nav>
-              <ul className={navLinks}>
-                <li className={navLinkItem}>
-                  <Link to="/" className={navLinkText}>
-                    Home
-                  </Link>
-                </li>
-                <li className={navLinkItem}>
-                  <Link to="/about" className={navLinkText}>
-                    About
-                  </Link>
-                </li>
-                <li className={navLinkItem}>
-                  <Link to="/blog" className={navLinkText}>
-                    Blog
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            <main>
-              <h1 className={heading}>{pageTitle}</h1>
-              {children}
-            </main>
-          </div>
-        </div>
+      <div className={container}>
+        <title>{pageTitle}</title>
+        <nav>
+          <ul className={navLinks}>
+            <li className={navLinkItem}>
+              <Link to="/" className={navLinkText}>
+                Home
+              </Link>
+            </li>
+            <li className={navLinkItem}>
+              <Link to="/blog" className={navLinkText}>
+                Blog
+              </Link>
+            </li>
+          </ul>
+        </nav>
+        <main>
+          <h1 className={heading}>{pageTitle}</h1>
+          {children}
+        </main>
+        <div className={`${outerBorder} ${decor}`}></div>
+        <div className={`${innerBorder} ${decor}`}></div>
       </div>
     </div>
   );
--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -6,14 +6,14 @@ import Layout from "../../components/layout";
 
 const BlogPage = ({ data }) => {
   return (
-    <Layout pageTitle="My Blog Posts">
+    <Layout pageTitle="Blog">
       <ul>
         {data.allMdx.nodes.map((node) => (
           <article key={node.id}>
             <h2>
               <Link to={`/blog/${node.slug}`}>{node.frontmatter.title}</Link>
             </h2>
-            <p>Posted: {node.frontmatter.date}</p>
+            <p>{node.frontmatter.date}</p>
           </article>
         ))}
       </ul>
