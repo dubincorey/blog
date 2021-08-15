dc0e54e28dd3b0f498c13e11f19ca72849991f18
Corey Dubin <dubincorey@gmail.com>
2021-08-15 11:43:44 -0400

    added blogs one and two

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
 import * as React from "react";
 import Layout from "../components/layout";
+import { disclaimer } from ".//layout.module.css";
 
 const IndexPage = () => {
   return (
     <Layout pageTitle="QuickFi">
       <p>
         I'm going to share my experiences and understanding of personal finance
-        and (mostly) credit cards!
+        and other interesting things to me!
       </p>
+      <h5 id={disclaimer}>
+        These are only my opinions. Everyone should practice their due diligence
+        when making a financial decision. Many financial decisions involve risk,
+        and people who practice those decisions should be aware of the risks
+        involved.
+      </h5>
     </Layout>
   );
 };
